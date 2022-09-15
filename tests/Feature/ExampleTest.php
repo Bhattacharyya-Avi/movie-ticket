6ee56694ea07fc95498f4e2740433846f67de7e8
Avindra Bhattacharyya <82871940+Bhattacharyya-Avi@users.noreply.github.com>
2022-09-15 09:56:49 +0600

    remove duplicates

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -4,7 +4,6 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use RefreshDatabase;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
